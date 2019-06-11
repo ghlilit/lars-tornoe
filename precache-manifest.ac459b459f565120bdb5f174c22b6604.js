eca76246379c92177bc2493c0fc89d70c00416bd
lilit-ghandilyan <lilit_ghandilyan@edu.aua.am>
2019-06-12 00:09:52 +0400

    Updates

---
+++ precache-manifest.ac459b459f565120bdb5f174c22b6604.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fcddd0e3a709686ff31bab6f517c39",
+    "url": "/ghlilit/lars-tornoe/index.html"
+  },
+  {
+    "revision": "80595852dd5d312cbad7",
+    "url": "/ghlilit/lars-tornoe/static/css/main.5d6ec9cc.chunk.css"
+  },
+  {
+    "revision": "453fb03ba9a02be9e108",
+    "url": "/ghlilit/lars-tornoe/static/js/2.a9d30fdd.chunk.js"
+  },
+  {
+    "revision": "80595852dd5d312cbad7",
+    "url": "/ghlilit/lars-tornoe/static/js/main.3e38e885.chunk.js"
+  },
+  {
+    "revision": "b0f5895abb50f4f4316f",
+    "url": "/ghlilit/lars-tornoe/static/js/runtime~main.2c9f4b3b.js"
+  },
+  {
+    "revision": "56f66ac36b8d7ed5ff8d31c782a8663b",
+    "url": "/ghlilit/lars-tornoe/static/media/news12.56f66ac3.jpg"
+  },
+  {
+    "revision": "34f597e2945253b29a7ea9fbe571e2ac",
+    "url": "/ghlilit/lars-tornoe/static/media/news13.34f597e2.jpg"
+  },
+  {
+    "revision": "89b01601a8f83adc3041c7054f6538e5",
+    "url": "/ghlilit/lars-tornoe/static/media/news15.89b01601.jpg"
+  },
+  {
+    "revision": "22a7732024a23dc07ed31ec3a725b012",
+    "url": "/ghlilit/lars-tornoe/static/media/news16.22a77320.jpg"
+  },
+  {
+    "revision": "fc9dc00e23e04f609719c637ef391c3a",
+    "url": "/ghlilit/lars-tornoe/static/media/news17.fc9dc00e.jpg"
+  },
+  {
+    "revision": "b22e6dbebbce97ac04c8785f4c89f363",
+    "url": "/ghlilit/lars-tornoe/static/media/news3.b22e6dbe.jpg"
+  },
+  {
+    "revision": "ef23a27087ce9611b8cf48aa611ec4a4",
+    "url": "/ghlilit/lars-tornoe/static/media/news4.ef23a270.jpg"
+  },
+  {
+    "revision": "33723cf3c100d8b1ce98995ca1fe2eba",
+    "url": "/ghlilit/lars-tornoe/static/media/news6.33723cf3.jpg"
+  },
+  {
+    "revision": "31fd81dd1213c0d124c163db33665df0",
+    "url": "/ghlilit/lars-tornoe/static/media/news8.31fd81dd.jpg"
+  },
+  {
+    "revision": "169a17abc14afa23bf54b765408022ea",
+    "url": "/ghlilit/lars-tornoe/static/media/news9.169a17ab.jpg"
+  },
+  {
+    "revision": "d9f95d574a76159166953464285813ee",
+    "url": "/ghlilit/lars-tornoe/static/media/tornoe.d9f95d57.jpg"
+  }
+]);
